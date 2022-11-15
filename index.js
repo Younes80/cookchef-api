10daebe94b58787a0639a289c4739e8ca9c17a38
Younes80 <57212570+Younes80@users.noreply.github.com>
2022-11-15 04:12:11 +0100

    Last version for prod

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express, { json } from 'express';
+// import bodyParser from 'body-parser';
 import RecipeRoutes from './src/routes/recipes.routes';
 import UserRoutes from './src/routes/users.routes';
 import FavorisRoutes from './src/routes/favoris.routes';
@@ -11,6 +12,12 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(json());
+
+app.use(express.static('./public'));
+app.use('/uploads', express.static('uploads'));
+
+app.use(express.json({ limit: '100mb', extended: true }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.use('/api/recipes', RecipeRoutes);
 app.use('/api/users', UserRoutes);
